863bbe0b19d74b0e7e801b6eda67e7d834601656
Sao I Kuan <saoikuan@gmail.com>
2022-09-26 20:05:28 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, Switch, Button } from 'react-native';
+import React, {useState} from "react";
 
 export default function App() {
+  
+  const bmi = (kg=157, cm=45) => {
+    bmi_result = (kg / ((cm / 100) ^ 2)).toFixed(2);
+    return bmi_result;
+  };
+
+  const bmiHandler = () =>{
+    let bmiOutput = new bmi()
+    return bmiOutput;
+  }
+
+  const [text, setText] = useState("BMI is ...");
+  const onPressHandler = event => setText({bmiHandler});
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text name='title' style={[styles.title]}>BMI Calculator</Text>
+
+      <Text name='titleHeight' style={[styles.title]}>Height:</Text>
+      <TextInput name='Height' style={styles.textbox}>157</TextInput>
+
+      <Text name='titleWeight' style={[styles.title]}>Weight: </Text>
+      <TextInput name='Weight' style={styles.textbox}>45</TextInput>
+
+      <Text name='titleResult' style={[styles.title]}>Result</Text>
+      <Text name='result' 
+            style={styles.title}
+            onChangeText = {bmiHandler}
+            >{text}</Text>
+
+      <Button title='Calculate'onPress={onPressHandler}></Button>
+      <Switch></Switch>
     </View>
   );
 }
@@ -16,5 +44,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor:'red'
   },
+  textbox: { 
+    borderColor: 'black', 
+    padding: 4,
+    borderWidth: '2px',
+    alignItems: "center" 
+  },
+  title: {
+    fontWeight: 'bold',
+    alignItems: 'center'
+  }
 });
